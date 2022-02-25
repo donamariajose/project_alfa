eae55e712c8ddd23bc7729fc99b01930895c4ba1
donamariajose <donadmj@gmail.com>
2022-02-25 16:28:25 +0530

    signup

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
+def signup(request):
+    return render(request, 'signup.html')
